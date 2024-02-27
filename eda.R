af4748f9aa3c6c1bc90b8f7e61a7c460edbba7a6
ikemelanson <ikemelanson@gmail.com>
2024-02-27 16:58:04 -0700

    Update eda.R
    add instructioncs

--- eda.R
+++ eda.R
@@ -1,2 +1,59 @@
 library(tidyverse)
 df <- read_csv("https://www.dropbox.com/scl/fi/2bbujng7y0dxpj8blb4ej/19_train.csv?rlkey=k8pxvbva2jwr0qp7oj1cc9ul5&dl=1")
+
+# 3. (12 points) Work as a group to perform an exploratory data analysis (EDA), in which you 
+  # generate a general understanding of the columns in the data. This can be in whatever format 
+  # you find useful, but you should integrate narrative text combined with applicable visualizations 
+  # and/or tables. Your narrative text should summarize what analyses you are performing, and note anything 
+  # interesting or concerning. This analysis writeup should include the following sections:
+# -------------------------------------------------------------------------------------------------------
+
+
+# A. (3 points) Examine the characteristics of the dependent variable that you’ll be training a
+  # model to predict. Look at the distribution and anything else that seems relevant, focusing 
+  # especially on anything that you’ll likely need to account for as you build your model(s). 
+  # Include one or more visualizations to illustrate the most important takeaway(s).
+
+
+
+
+# -------------------------------------------------------------------------------------------------------
+# B. (4 points) Summarize the characteristics of the remaining variables in the dataset at a high level. 
+  # This should include any relevant groupings of like variables, a description of the number of 
+  # categorical vs. continuous variables, any relevant patterns of missingness or odd distributions, etc.
+  # This should not be a long, comprehensive section of your report. What we’re looking for here is a high-level 
+  # sketch of the dataset so that, in a page or two, the reader can get an idea of the data that you’ll be using
+  # and anything in particular that you’ll need to watch out for as you progress to building your model. (Note that,
+  # even though I’m asking for just a page or two of column summary, your team will likely need to look fairly carefully
+  # at each of the columns in order to identify any lurking issues and know which columns to highlight, etc.)
+
+
+
+
+
+
+
+# -------------------------------------------------------------------------------------------------------
+# C. (5 points) Perform an initial analysis in which you systematically examine each independent 
+  # variable as it relates to your dependent variable. This will provide your group with an initial
+  # idea of which (if any) variable jump out as especially promising features.
+
+
+  # - For numeric (continuous) variables, this will likely involve looking at correlations or something
+  # similar in order to identify variables (if any) that are likely related to your dependent variable.
+  # Any particularly interesting continuous variables may deserve a scatterplot or some other visual 
+  # demonstration of the relationship.
+
+
+
+  # - For categorical variables, this will involve looking at the pattern of the dependent 
+  # variable at various levels of the category. This will look different depending on your dependent 
+  # variable, but what you’re looking to do is highlight any categorical variables where the dependent 
+  # variable appears “different” (in terms of rate, frequency, average, etc.) at different levels of that 
+  # category. Summarize anything you find particularly interesting or relevant using an appropriate visualization.
+
+
+
+
+
+
